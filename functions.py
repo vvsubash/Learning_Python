8ad96842763d32f4b4c13f66a5710e473361d592
venkat <subash.venkat007@gmail.com>
2021-11-26 06:29:11 +0000

    Initial commit

---
+++ functions.py
@@ -0,0 +1,11 @@
+def print_name(name):
+  print(f"my name is {name}")
+
+print_name("subash")
+
+def increment(number,by):
+  return number + by
+
+five = increment(3,2)
+
+print(five)
---
+++ variables.py
@@ -0,0 +1,7 @@
+# In Pyrhon there are no special key words for declaring variables It's Just
+name = "Subash"
+string = "String"
+intiger = "3"
+floating_point_number = "3.366"
+boolean = True
+# Complex numbers can be declared by importing cmath
